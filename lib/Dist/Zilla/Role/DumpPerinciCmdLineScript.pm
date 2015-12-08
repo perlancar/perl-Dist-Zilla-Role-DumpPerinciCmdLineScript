83baf8119228a92beda5a670ad9123b27ce10b2c
perlancar (@pc-office) <perlancar@gmail.com>
2015-12-08 16:34:06 +0700

    Set environment DZIL=1 to let script be aware it's being dumped in Dist::Zilla build context, add logging

--- lib/Dist/Zilla/Role/DumpPerinciCmdLineScript.pm
+++ lib/Dist/Zilla/Role/DumpPerinciCmdLineScript.pm
@@ -23,6 +23,12 @@ sub dump_perinci_cmdline_script {
         $filename = $tempname;
     }
 
+    # so scripts can know that they are being dumped in the context of
+    # Dist::Zilla
+    local $ENV{DZIL} = 1;
+
+    $self->log_debug(["Dumping Perinci::CmdLine script '%s'", $filename]);
+
     Perinci::CmdLine::Dump::dump_perinci_cmdline_script(
         filename => $filename,
         libs => ['lib'],
@@ -42,4 +48,3 @@ no Moose::Role;
 
 L<Pod::Weaver::Role::DumpPerinciCmdLineScript> basically does the same thing,
 but it accepts a slightly different argument (C<$input> instead of C<$file>).
-
